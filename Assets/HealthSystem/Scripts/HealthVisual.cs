cee7ec1686410579446c1c24f7ad5ea21a47633b
Sindise <X.MUSTACHEWAR@GMAIL.COM>
2026-04-29 09:55:34 +0200

    Health visual 1

--- Assets/HealthSystem/Scripts/HealthVisual.cs
+++ Assets/HealthSystem/Scripts/HealthVisual.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-public class HealthVisual : MonoBehaviour { 
+using UnityEngine.UI;
+public class HealthVisual : MonoBehaviour 
+{
 
+    [SerializeField] private Sprite heartSprite;
+    private Image CreateHeartImage() { 
+        GameObject heartGameObject = new GameObject("Heart", typeof(Image));
+        heartGameObject.transform.parent = transform;
+        heartGameObject.transform.localPosition = Vector3.zero;
+        Image heartImage = heartGameObject.GetComponent<Image>();
+        heartImage.sprite = heartSprite;
+
+        return heartImage;
+    }
 }
